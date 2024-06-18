6225fd924136c29fe90882479185f3bb552d5139
TF1405 <ferdiyanata@gmail.com>
2024-06-18 18:01:20 +0700

    [FEAT] Add Seed File for OrderItems, Order, Carts, and Wishlists Table

--- backend/seeders/20240617095704-demo-orders.cjs
+++ backend/seeders/20240617095704-demo-orders.cjs
@@ -1,25 +1,21 @@
 'use strict';
+const { v4: uuidv4 } = require('uuid');
 
-/** @type {import('sequelize-cli').Migration} */
 module.exports = {
-  async up (queryInterface, Sequelize) {
-    /**
-     * Add seed commands here.
-     *
-     * Example:
-     * await queryInterface.bulkInsert('People', [{
-     *   name: 'John Doe',
-     *   isBetaMember: false
-     * }], {});
-    */
-  },
+	async up(queryInterface, Sequelize) {
+		await queryInterface.bulkInsert('Orders', [
+			{
+				order_id: uuidv4(),
+				user_id: '30644cd2-502f-4600-a09d-bc8d7901b018',
+				total_amount: 178000,
+				status: 'Shipped',
+				createdAt: new Date(),
+				updatedAt: new Date(),
+			},
+		]);
+	},
 
-  async down (queryInterface, Sequelize) {
-    /**
-     * Add commands to revert seed here.
-     *
-     * Example:
-     * await queryInterface.bulkDelete('People', null, {});
-     */
-  }
+	async down(queryInterface, Sequelize) {
+		await queryInterface.bulkDelete('Orders', null, {});
+	},
 };
--- backend/seeders/20240617095711-demo-order-items.cjs
+++ backend/seeders/20240617095711-demo-order-items.cjs
@@ -1,25 +1,12 @@
 'use strict';
+const { v4: uuidv4 } = require('uuid');
 
-/** @type {import('sequelize-cli').Migration} */
 module.exports = {
-  async up (queryInterface, Sequelize) {
-    /**
-     * Add seed commands here.
-     *
-     * Example:
-     * await queryInterface.bulkInsert('People', [{
-     *   name: 'John Doe',
-     *   isBetaMember: false
-     * }], {});
-    */
-  },
+	async up(queryInterface, Sequelize) {
+		await queryInterface.bulkInsert('OrderItems', [{}]);
+	},
 
-  async down (queryInterface, Sequelize) {
-    /**
-     * Add commands to revert seed here.
-     *
-     * Example:
-     * await queryInterface.bulkDelete('People', null, {});
-     */
-  }
+	async down(queryInterface, Sequelize) {
+		await queryInterface.bulkDelete('OrderItems', null, {});
+	},
 };
