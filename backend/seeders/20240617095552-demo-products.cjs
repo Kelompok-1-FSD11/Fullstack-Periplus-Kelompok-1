4b29b414aa876f9b87a57d3d3251001926843b3f
TF1405 <ferdiyanata@gmail.com>
2024-06-18 10:42:24 +0700

    [FEAT] Add Seed file

--- backend/seeders/20240617095552-demo-products.cjs
+++ backend/seeders/20240617095552-demo-products.cjs
@@ -11,7 +11,7 @@ module.exports = {
 				product_description: null,
 				price: 200000,
 				qty_stock: 20,
-				category_id: 'a1492c0f-3dc2-4055-92db-411975e12b7e',
+				category_id: 'a6153621-b61a-48c9-81d0-ff61f5b89c48',
 				qty_sold: 1,
 				image_path:
 					'https://static.periplus.com/u1I24YeCrPKqOnWy3zdfs2R56oTxzkuc0JFQnBfXIMsY9gHCa3kvOhGePCg52vvkA--',
@@ -24,7 +24,7 @@ module.exports = {
 				product_description: null,
 				price: 201000,
 				qty_stock: 10,
-				category_id: 'a1492c0f-3dc2-4055-92db-411975e12b7e',
+				category_id: 'a6153621-b61a-48c9-81d0-ff61f5b89c48',
 				qty_sold: 0,
 				image_path:
 					'https://static.periplus.com/gMMZAF5AMdXqmkFLHj3z5SkhOFsb6oUrBr8CIAYrgleO1Wqzn5NpWZ2yoyxLDdqMw--',
@@ -37,7 +37,7 @@ module.exports = {
 				product_description: null,
 				price: 178000,
 				qty_stock: 6,
-				category_id: 'a1492c0f-3dc2-4055-92db-411975e12b7e',
+				category_id: 'a6153621-b61a-48c9-81d0-ff61f5b89c48',
 				qty_sold: 2,
 				image_path:
 					'https://static.periplus.com/ioEiqXwe3HqDZrnfIFvRMPlvGf_FeYC4LDunhB9bq3iIJyuyQ8FvVtZJnncRyllNQ--',
