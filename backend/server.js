7c4d8c376da202145c05702d430cd3a6f93d9cef
virgiawanara <agungvirgiawan13@gmail.com>
2024-06-29 00:42:05 +0700

    add cors for server.js

--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,40 @@
-import express from "express";
-import cors from "cors";
+import express from 'express';
+import dotenv from 'dotenv';
 import publicRoute from './routes/publicRoute.js';
 import userRoute from './routes/userRoute.js';
 import adminRoute from './routes/adminRoute.js';
+import importModels from './models/index.js';
+import errorHandler from './middlewares/errorHandler.js';
+import cors from "cors";
 
-const app = express();
-app.use(cors());
-app.use(express.json());
-app.use('/api', publicRoute);
-app.use('/api/user', userRoute);
-app.use('/api/admin', adminRoute);
+dotenv.config();
 
-const PORT = process.env.PORT || 5000;
+const startServer = async () => {
+	try {
+		const db = await importModels();
 
-app.listen(PORT, ()=> console.log(`Server up and running on port ${PORT}`));
+		const app = express();
+
+		app.use(cors());
+		
+		app.use(express.json());
+
+		app.use('/api', publicRoute);
+		app.use('/api/user', userRoute);
+		app.use('/api/admin', adminRoute);
+
+		app.use(errorHandler);
+
+		const PORT = process.env.PORT || 5000;
+
+		db.sequelize.sync().then(() => {
+			app.listen(PORT, () => {
+				console.log(`Server is running on port ${PORT}`);
+			});
+		});
+	} catch (error) {
+		console.error('Unable to start the server:', error);
+	}
+};
+
+startServer();
